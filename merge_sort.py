547f88be1dd480379b3194848be58a0df724a35f
Ashutosh Anand <55953959+ashutoshanand139@users.noreply.github.com>
2023-01-17 22:35:30 +0530

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -2,31 +2,31 @@
 
 def merge(left, right):
     res = []
-    i,j = 0,0
-    
-    while i<len(left) and j<len(right):
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
         if left[i] < right[j]:
             res.append(left[i])
-            i+=1
+            i += 1
         else:
             res.append(right[j])
-            j+=1
-            
+            j += 1
+
     res.extend(left[i:])
     res.extend(right[j:])
     return res
-    
+
+
 def mergeSort(arr):
     if len(arr) <= 1:
         return arr
-    
-    mid = len(arr)//2
+
+    mid = len(arr) // 2
     left = mergeSort(arr[:mid])
     right = mergeSort(arr[mid:])
-    
-    return merge(left, right)
-    
-a = [56,1,3,4,5,3,3,3,67,789,3456,1,9,6,3,34,23,0,-5,-10,-3,57]
-print(mergeSort(a))
 
-        
+    return merge(left, right)
+
+
+a = [56, 1, 3, 4, 5, 3, 3, 3, 67, 789, 3456, 1, 9, 6, 3, 34, 23, 0, -5, -10, -3, 57]
+print(mergeSort(a))
